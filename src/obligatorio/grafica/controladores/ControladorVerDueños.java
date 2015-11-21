b862c8ea5b5f92c75acdff0731b738e3b1c296b1
se.gonzalez <se.gonzalez@globant.com>
2015-11-21 19:15:54 -0300

    arreglando ver dueños

--- src/obligatorio/grafica/controladores/ControladorVerDueños.java
+++ src/obligatorio/grafica/controladores/ControladorVerDueños.java
@@ -1,5 +1,7 @@
 package obligatorio.grafica.controladores;
 
+import java.util.List;
+
 import obligatorio.exceptions.LogicaException;
 import obligatorio.exceptions.PersistenciaException;
 import obligatorio.logica.Fachada;
@@ -9,23 +11,20 @@ public class ControladorVerDueños {
 
 	public Object[][] listarDueños() throws LogicaException,
 			PersistenciaException {
-
 		Object[][] data = null;
-
-		VODueño[] dueños = (VODueño[]) Fachada.getInstance().listarDueños()
-				.toArray();
-
-		if (dueños.length != 0) {
-
-			data = new Object[dueños.length][3];
-
-			for (int i = 0; i < dueños.length; i++) {
-				data[i][0] = new Integer(dueños[i].getCedula());
-				data[i][1] = new String(dueños[i].getNombre());
-				data[i][2] = new String(dueños[i].getApellido());
+		
+		List<VODueño> dueños = Fachada.getInstance().listarDueños();
+		int total = dueños.size();
+		
+		if (total != 0) {
+			data = new Object[total][3];
+			for (int i = 0; i < total; i++) {
+				data[i][0] = new Integer(dueños.get(i).getCedula());
+				data[i][1] = new String(dueños.get(i).getNombre());
+				data[i][2] = new String(dueños.get(i).getApellido());
 			}
 		} else {
-
+			//TODO: no se puede devolver algo mejor? una exception o algo?
 			data = new Object[1][3];
 			data[0][0] = "";
 			data[0][1] = "";
@@ -34,5 +33,4 @@ public class ControladorVerDueños {
 
 		return data;
 	}
-
 }
